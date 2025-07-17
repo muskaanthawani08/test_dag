566152f66c499ebec7480af52dd0b0b10548b265
Muskaan Thawani <muskaan.thawani@tresvista.com>
2025-07-17 09:57:45 +0530

    !@@

--- dags/dag.py
+++ dags/dag.py
@@ -15,8 +15,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # File paths
+FILE_PATH = os.path.join(os.path.dirname(__file__), 'include', 'supermarket_sales.csv')
 
-FILE_PATH = '/usr/local/airflow/include/supermarket_sales.csv'
 # Logging setup
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
