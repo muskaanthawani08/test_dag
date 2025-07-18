5b0667c7567158a41ef0ebd733b156e85ad5190d
Muskaan Thawani <muskaan.thawani@tresvista.com>
2025-07-18 11:39:15 +0530

    Insert

--- dags/dag.py
+++ dags/dag.py
@@ -149,7 +149,7 @@ def load_data():
 
         # Create table if not exists
         create_stmt = """
-        CREATE TABLE IF NOT EXISTS DAILY_SALES (
+        CREATE TABLE IF NOT EXISTS DAILY_SALES_OP (
             INVOICE_ID STRING,
             STORE STRING,
             CITY STRING,
@@ -246,14 +246,14 @@ validate_task = PythonOperator(
     dag=dag
 )
  
-def skip_if_empty(**kwargs):
-    try:
-        is_empty = kwargs['ti'].xcom_pull(task_ids='validate_sales_file', key='is_empty')
-        logging.info(f"Branch decision: is_empty={is_empty}")
-        return 'skip_load' if is_empty else 'transform_sales_data'
-    except Exception as e:
-        logging.error(f"Error in skip_if_empty: {e}")
-        raise
+# def skip_if_empty(**kwargs):
+#     try:
+#         is_empty = kwargs['ti'].xcom_pull(task_ids='validate_sales_file', key='is_empty')
+#         logging.info(f"Branch decision: is_empty={is_empty}")
+#         return 'skip_load' if is_empty else 'transform_sales_data'
+#     except Exception as e:
+#         logging.error(f"Error in skip_if_empty: {e}")
+#         raise
  
  
 branch_task = BranchPythonOperator(
